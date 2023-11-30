3a880a75f60ecba7f6a4e3b748bb8ab7d71e0874
surfacez <sergiobatistajr94@gmail.com>
2023-11-30 14:42:28 -0300

    Refactor import statements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import express from "express"
-import { TaskController } from "./controllers/TaskController"
-import { UserController } from "./controllers/UserController"
+import TaskController from "./controllers/TaskController"
+import UserController from "./controllers/UserController"
 import { authMiddleware as auth } from "./middleware"
 const app = express()
-const taskController = new TaskController()
-const userController = new UserController()
 // Middleware para parsear o corpo das requisições
 app.use(express.json())
 
 // Rotas
-app.post("/register", (req, res) => userController.postRegister(req, res))
-app.post("/login", (req, res) => userController.postLogin(req, res))
-app.post("/tasks", auth, (req, res) => taskController.postTask(req, res))
-app.put("/tasks/:id", auth, (req, res) => taskController.putTask(req, res))
-app.get("/tasks", auth, (req, res) => taskController.getTasks(req, res))
+app.post("/register", (req, res) => UserController.postRegister(req, res))
+app.post("/login", (req, res) => UserController.postLogin(req, res))
+app.post("/tasks", auth, (req, res) => TaskController.postTask(req, res))
+app.put("/tasks/:id", auth, (req, res) => TaskController.putTask(req, res))
+app.get("/tasks", auth, (req, res) => TaskController.getTasks(req, res))
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 3000
